2feb16f0c5b928c2aa65a5a80cd5fd51f67f3066
Users <juan.figuera@correo.unimet.edu.vegit config --global user.name Juan>
2024-02-09 16:48:05 -0400

    Registro

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 50) ;
+    return Math.floor(Math.random() * 50)+1 ;
 }
 
 function generarTablero(tableroId,size){
@@ -18,8 +18,35 @@ function generarTablero(tableroId,size){
     }
 }
 
+function sacarNumero(){
+    let numero = getRandomNumber();
+}
+    let numero = getRandomNumber();
 
-generarTablero("Tab1",3);
-generarTablero("Tab2",3);
-generarTablero("Tab3",3);
-generarTablero("Tab4",3);
+
+function registrarNombres(){
+    if (document.getElementById('Nombre1').innerText == null) {
+        alert("Ingrese los nombres");
+        
+    }else{
+        let jugador = document.getElementById("Nombre1").value;
+        document.getElementById("Jugador1").innerText = jugador
+        let jugador2 = document.getElementById("Nombre2").value;
+        document.getElementById("Jugador2").innerText = jugador2
+        let jugador3 = document.getElementById("Nombre3").value;
+        document.getElementById("Jugador3").innerText = jugador3
+        let jugador4 = document.getElementById("Nombre4").value;
+        document.getElementById("Jugador4").innerText = jugador4
+        let num = document.getElementById("tam").value
+        generarTablero("Tab1",num);
+        generarTablero("Tab2",num);
+        generarTablero("Tab3",num);
+        generarTablero("Tab4",num);
+    }
+    
+}
+
+
+
+
+
